c438425e03cf22a4f83a2f2c50d22f97632a6a80
Hayzerr <bolashak040206@mail.ru>
2025-09-20 13:05:47 +0500

    first commit

---
+++ app/src/main/java/com/example/profilecard/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.profilecard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.profilecard.ui.theme.ProfileCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProfileCardTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ProfileCard(
+                        name = "Bolashak Kulmukhambetov",
+                        bio = "Android Developer",
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfileCard(
+    name: String,
+    bio: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+                .background(Color.Cyan)
+                .padding(32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Avatar",
+                modifier = Modifier.size(100.dp)
+            )
+
+            Spacer(Modifier.height(8.dp))
+
+            Text(text = name, style = MaterialTheme.typography.titleMedium)
+            Text(text = bio, style = MaterialTheme.typography.bodyMedium)
+
+            Spacer(Modifier.height(12.dp))
+
+            Button(onClick = { }) {
+                Text("Follow")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProfileCardTheme {
+        ProfileCard(
+            name = "Bolashak Kulmukhambetov",
+            bio = "Android Developer"
+        )
+    }
+}
