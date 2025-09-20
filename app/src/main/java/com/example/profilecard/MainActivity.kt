ff0cd189e38954418541ac1590e51909c24875d2
Hayzerr <bolashak040206@mail.ru>
2025-09-20 13:13:52 +0500

    pfp added

--- app/src/main/java/com/example/profilecard/MainActivity.kt
+++ app/src/main/java/com/example/profilecard/MainActivity.kt
@@ -8,10 +8,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,14 +54,16 @@ fun ProfileCard(
     ) {
         Column(
             modifier = Modifier.padding(16.dp)
+                .clip(RoundedCornerShape(16.dp))
                 .background(Color.Cyan)
                 .padding(32.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                painter = painterResource(id = R.drawable.avatar),
                 contentDescription = "Avatar",
                 modifier = Modifier.size(100.dp)
+                    .clip(CircleShape)
             )
 
             Spacer(Modifier.height(8.dp))
